98905cad97687694d463321cc4bc14fa5ec9e399
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2016-05-30 11:04:51 +0300

    Added environment and version for self-recognition

---
+++ src/com/mymoney/entities/AppVersion.java
@@ -0,0 +1,9 @@
+package com.mymoney.entities;
+
+public class AppVersion {
+public static String coreVersion = "0.0.1 BUILD 4";
+public static String profilingEngineVersion = "0.0.1 BUILD 0";
+public static String databaseVersion = "0.0.1 BUILD 1";
+public static String environment = "DEV";
+
+}
